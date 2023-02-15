982578ba0be70fe1096e8b8952cff0f78731ae69
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-15 17:34:09 +0530

    Added:UC1 checking atandence

---
+++ Attendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageBYArry
+{
+    public class Attendance
+    {
+        public void  attendance()
+        {
+         Random obj = new Random(); 
+         int check =obj.Next(0,2);
+
+            if (check==0) 
+            {
+                Console.WriteLine("absent");
+            }
+            else 
+            {
+                Console.WriteLine("prsent");
+            }
+        
+        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWageBYArry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Computation");
         }
     }
 }
