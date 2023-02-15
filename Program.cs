9a2958b2e2c7f8fbac10b6fabd592ded9654d78d
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-15 17:03:50 +0530

    Added: Employee Wage Computation program filie

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageBYArry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
